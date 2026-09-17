14bc00ff64abc6f8ab2334dba80087878c549617
Jacek <jacekfenske@proton.me>
2026-09-17 17:17:05 +0100

    Einige Funktionen nach Individuum verschoben

--- Evolution.py
+++ Evolution.py
@@ -1,14 +1,58 @@
 import random
+import math
 
 class Individuum:
-  fitness: float = 0
+  _fitness: float = 0
 
   def __init__(self, dna: list[float]):
     self._dna = dna
+    # Sicherstellen, dass eine gültige DNA eingegeben wurde.
 
-  @property
+  def erstelle_dna(self):
+    dna = []
+    for i in range(4):
+      dna.append(random.randint(0, 50))
+
+    self._dna = dna
+
+  def teste_fitness(self):
+    durchschnitt = 0
+    for i in self.dna:
+      durchschnitt += i
+    durchschnitt = durchschnitt / len(self.dna)
+
+    fitness = 0
+    for i in self.dna:
+      fitness -= abs(durchschnitt - i)
+
+    self._fitness = fitness
+
+  def erstelle_mutation(slef):
+    dna = []
+    n = 0
+    for i in slef.dna():
+      u1 = random.random()
+      u2 = random.random()
+      z = math.sqrt(-2 * math.log(u1)) * math.cos(2 * math.pi * u2)
+
+      dna.append(slef.dna()[n] + z)
+
+      n += 1
+
+    for i in dna:
+      if i < 0:
+        dna[dna.index(i)] = 0
+
+      if i > 50:
+        dna[dna.index(i)] = 50
+
+    return Individuum(dna)
+
   def dna(self):
     return self._dna
+
+  def fitness(self):
+    return self._fitness
 
 class Generation:
   _individuen: list[Individuum] = []
@@ -18,16 +62,3 @@ class Generation:
 
   def append_individuum(self, individuum: Individuum):
     self._individuen.append(individuum)
-
-def erstelle_dna():
-  dna = []
-  for i in range(4):
-    dna.append(random.randint(0, 50))
-  return dna
-
-def teste_fitness(individuum: Individuum):
-  durchschnitt = 0
-  for i in individuum.dna:
-    durchschnitt += i
-  durchschnitt = durchschnitt / len(individuum.dna)
-  # Nähe der einzelnen DNAteile zum Durchschnitt messen.
